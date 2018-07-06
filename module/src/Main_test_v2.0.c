400613fc7042fa9fdb1191287c3469765cef9a67
Unknown <florian.meyer@edu.hefr.ch>
2018-07-06 11:42:31 +0200

    algo regul look working. But back the service

--- module/src/Main_test_v2.0.c
+++ module/src/Main_test_v2.0.c
@@ -1166,7 +1166,7 @@ void Time_init(void)
 int main()
 {
 
-	/*// 1. fork off the parent process
+	// 1. fork off the parent process
 	fork_process();
 
 	// 2. create new session
@@ -1213,7 +1213,7 @@ int main()
 	if (dup2 (STDIN_FILENO, STDERR_FILENO) != STDERR_FILENO) {
 		syslog (LOG_ERR, "ERROR while opening '/dev/null' for stderr");
 		exit (1);
-	}*/
+	}
   	MQTTClient  client_charger;
   	MQTTClient_connectOptions conn_opts_charger = MQTTClient_connectOptions_initializer;
 
