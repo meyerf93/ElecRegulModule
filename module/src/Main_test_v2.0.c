f7c190a738ddefd1af4c9f90eea255860cc3e9d2
Unknown <florian.meyer@edu.hefr.ch>
2018-07-09 16:10:33 +0200

    remove service

--- module/src/Main_test_v2.0.c
+++ module/src/Main_test_v2.0.c
@@ -1145,7 +1145,7 @@ void Time_init(void)
 
 
 int main()
-{
+{/*
 	// 1. fork off the parent process
 	fork_process();
 
@@ -1193,7 +1193,7 @@ int main()
 	if (dup2 (STDIN_FILENO, STDERR_FILENO) != STDERR_FILENO) {
 		syslog (LOG_ERR, "ERROR while opening '/dev/null' for stderr");
 		exit (1);
-	}
+	}*/
   	MQTTClient  client_charger;
   	MQTTClient_connectOptions conn_opts_charger = MQTTClient_connectOptions_initializer;
 
