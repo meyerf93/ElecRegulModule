8700932bde4cc59bee3e5e4d935dac9146186437
Unknown <florian.meyer@edu.hefr.ch>
2018-07-18 10:43:01 +0200

    bug

--- module/include/connection.h
+++ module/include/connection.h
@@ -22,6 +22,7 @@
 #include <syslog.h>
 
 #include <json.h>
+;
 #include <MQTTClient.h>
 #include "cJSON.h"
 
--- module/include/conversion.h
+++ module/include/conversion.h
@@ -22,8 +22,10 @@
 #include <syslog.h>
 
 #include "cJSON.h"
+;
 #include <json.h>
 #include <MQTTClient.h>
+
 
 #define BOOL_F 1
 #define FLOAT_F 6
@@ -31,7 +33,7 @@
 #define LONG_ENUM 10
 #define SHORT_ENUM 11
 #define ENUM 3
-;
+
 void send_json_obj(MQTTClient client,char topic[64], char data[64], char mdl[64], char id[64]);
 
 int parse_energy_meters(char* payload);
