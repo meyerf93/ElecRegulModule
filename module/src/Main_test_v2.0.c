9f6ab049b68965fd973bfa508bdf6169c7fb9a23
Unknown <florian.meyer@edu.hefr.ch>
2018-07-20 09:52:43 +0200

    remove service

--- module/src/Main_test_v2.0.c
+++ module/src/Main_test_v2.0.c
@@ -747,7 +747,8 @@ int main()
 	if (dup2 (STDIN_FILENO, STDERR_FILENO) != STDERR_FILENO) {
 		syslog (LOG_ERR, "ERROR while opening '/dev/null' for stderr");
 		exit (1);
-	}*/
+	}
+	*/
 
 	// end of the code for the service
 
