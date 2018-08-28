9eb3e78f1789f0b8212602b9c7bab7658f014f18
Unknown <florian.meyer@edu.hefr.ch>
2018-08-28 10:25:40 +0200

    remove json delete

--- module/src/connection.c
+++ module/src/connection.c
@@ -82,7 +82,7 @@ int msgarrvd(void *context, char *topicName, int topicLen, MQTTClient_message *m
     printf("data parsed : %s\n",cJSON_Print(data));
 
     cJSON_Delete(data);
-    cJSON_Delete(root);
+    //cJSON_Delete(root);
     printf("palyoad parsed after clean data 1 : %s\n",cJSON_Print(root));
     printf("data parsed after cleanm data 1: %s\n",cJSON_Print(data));
 
